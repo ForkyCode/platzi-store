a5c0ded58a2fde0d11fa5d0ab9faafb2eaed3d1e
ForkyCode <jeremichacon2000@gmail.com>
2025-08-26 20:13:51 -0500

    Se realiza la carpeta de controllers y se crean los primeros controladores para la app.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return 'Hola mundo!';
-  }
+@Get()
+getHello(): string {
+  return 'Hola mundo!';
+ }
+
+@Get('nuevo')
+newEndpoint() {
+  return 'Hola nuevo endpoint';
+ }
+
+@Get('products')
+getProducts(
+  @Query('limit') limit = 100,
+  @Query('offset') offset = 0,
+  @Query('brand') brand: string,
+) {
+  return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+}
+
+@Get('products/filter')
+getProductFilter() {
+  return `soy un filtro`;
+ }
+
+@Get('products/:productId')
+getProduct(@Param('productId') productId: string) {
+  return `product ${productId}`;
+ }
+
+@Get('categories/:id/products/:productId')
+getCategory(@Param('productId') productId: string, 
+            @Param('id') id: string) {
+  return `product ${productId} and ${id}`;
+ }
 }
