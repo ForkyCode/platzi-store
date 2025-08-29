b245b1727c5a492c6758c8af1670314bb722e7a5
ForkyCode <jeremichacon2000@gmail.com>
2025-08-28 21:52:46 -0500

    cambios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,38 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-@Get()
-getHello(): string {
-  return 'Hola mundo!';
- }
+  @Get()
+  getHello(): string {
+    return 'Hola mundo!';
+  }
 
-@Get('nuevo')
-newEndpoint() {
-  return 'Hola nuevo endpoint';
- }
-
-@Get('products')
-getProducts(
-  @Query('limit') limit = 100,
-  @Query('offset') offset = 0,
-  @Query('brand') brand: string,
-) {
-  return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
-}
-
-@Get('products/filter')
-getProductFilter() {
-  return `soy un filtro`;
- }
-
-@Get('products/:productId')
-getProduct(@Param('productId') productId: string) {
-  return `product ${productId}`;
- }
-
-@Get('categories/:id/products/:productId')
-getCategory(@Param('productId') productId: string, 
-            @Param('id') id: string) {
-  return `product ${productId} and ${id}`;
- }
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Hola nuevo endpoint';
+  }
 }
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `soy un filtro`;
+  }
+
+  @Get(':productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+}
